c96275c6dd6a8e875b3c23ff9865e31d60b16b99
tbelle0825 <tbelle0825@gmail.com>
2015-06-15 09:52:11 +0900

    class-3days

---
+++ jQuery-Class-DAY03/www/js/main2.js
@@ -0,0 +1,47 @@
+/**
+ * --------------------------------
+ * require([의존파일리스트], 콜백함수)
+ * --------------------------------
+ */
+
+/**
+ * --------------------------------
+ * RequireJS 환경설정
+ * --------------------------------
+ */
+
+
+require.config({
+	baseUrl: 'js',
+	paths: {
+		'jquery': 'libs/jquery.min',
+		'isJquery' : 'modules/isJquery',
+		'isJqueryVersion': 'modules/isJqueryVersion',
+		'assignBgImageforPage': 'modules/assignBgImageforPage'
+	}
+});
+
+/**
+ * DOMScript Code
+ * 별도의 모듈 호출 X
+ * --------------------------------
+ */
+// require([], function() {
+// 	var body = document.body;
+// 	body.classList.add('no-assign-jquery');
+// 	body.style.height = window.innerHeight + 'px';
+// 	body.setAttribute('data-body', 'root');
+// });
+
+/**
+ * jQuery Code
+ * jQuery 모듈 호출
+ * --------------------------------
+ */
+//모듈 호출
+require(['assignBgImageforPage'], function($, isJquery, isJqueryVersion) {
+	
+	//console.log(isJquery(), isJqueryVersion);
+});
+
+
